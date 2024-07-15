6b087c1f57684c242ac1cc9449db38a35f3e33b3
Gr1stonqqq <sashatroan@gmail.com>
2024-07-15 15:21:31 +0300

    Added new test

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,6 +6,7 @@ class TestMain(unittest.TestCase):
         self.assertEqual(add(1, 2), 3)
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
+        self.assertEqual(add(5, 4), 9)
 
 if __name__ == "__main__":
     unittest.main()
