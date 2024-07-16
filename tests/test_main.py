8eebdf048ec11a42a47ac42363b711cabd04131b
Gr1stonqqq <sashatroan@gmail.com>
2024-07-16 11:05:43 +0300

    Added adutional tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,6 +7,7 @@ class TestMain(unittest.TestCase):
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
         self.assertEqual(add(5, 4), 9)
+        self.assertEqual(add(5, 19), 24)
 
 if __name__ == "__main__":
     unittest.main()
